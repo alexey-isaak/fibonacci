555e250f4bf49ffb03c36fd3b4dba75d641f0f43
alex-isaak <122435364+alex-isaak@users.noreply.github.com>
2023-02-13 12:08:37 +0300

    Final script recursive

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//f(n) = f(n-1) + f(n-2)
+
+int fibonacci(int n)
+{
+    if( n == 1 || n ==2 ) return 1;
+    else return fibonacci(n-1) + fibonacci(n-2);
+}
+
+for (int i =1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) =  {fibonacci(i)}");
+}
+
